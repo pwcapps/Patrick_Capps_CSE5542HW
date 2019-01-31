aeac8bc8153b8bc15334f9818791530751715389
Patrick Capps <patrick.capps0@gmail.com>
2019-01-31 12:23:07 -0500

    finished bot

--- Assignment1/js/scene.js
+++ Assignment1/js/scene.js
@@ -6,8 +6,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
+camera.position.y = 2;
+camera.rotation.x = -Math.PI / 8;
 var geometry = new THREE.PlaneGeometry(3, 3, 5, 5);
-var material = new THREE.MeshNormalMaterial();
+var material = new THREE.MeshBasicMaterial();
 material.wireframe = true;
 var group = new THREE.Group();
 scene.add(group);
@@ -18,54 +20,112 @@ object.position.y = -2
 object.rotation.x = - Math.PI / 2;
 group.add(object);
 
+material = new THREE.MeshNormalMaterial();
+material.wireframe = true;
 // Create legs
-geometry = new THREE.BoxGeometry(.5, .5, 1);
+geometry = new THREE.BoxGeometry(.3, .3, 1);
 object = new THREE.Mesh(geometry, material);
 object.position.x = .5;
-object.position.y = -1.5;
-object.position.z = 1;
+object.position.y = -1.85;
+object.position.z = .2;
 group.add(object);
 
-// geometry = new THREE.BoxGeometry(.5, .5, 1);
-// object = new THREE.Mesh(geometry, material);
-// object.position.x = -.5;
-// object.position.y = -1.5;
-// object.position.z = 1;
-// group.add(object);
-//
-// geometry = new THREE.BoxGeometry(.5, 1, .5);
-// object = new THREE.Mesh(geometry, material);
-// object.position.x = -.5;
-// object.position.y = -1.5;
-// // object.position.z = 1;
-// group.add(object);
-//
-// geometry = new THREE.BoxGeometry(.5, 1, .5);
-// object = new THREE.Mesh(geometry, material);
-// object.position.x = .5;
-// object.position.y = -1;
-// // object.position.z = 1;
-// group.add(object);
+geometry = new THREE.BoxGeometry(.3, .3, 1);
+object = new THREE.Mesh(geometry, material);
+object.position.x = -.5;
+object.position.y = -1.85;
+object.position.z = .2;
+group.add(object);
 
+geometry = new THREE.BoxGeometry(.3, 1, .3);
+object = new THREE.Mesh(geometry, material);
+object.position.x = .5;
+object.position.y = -1.2;
+group.add(object);
 
-// geometry = new THREE.SphereGeometry(1);
-// var sphere = new THREE.Mesh(geometry, material);
-// scene.add(sphere);
-//
-// geometry = new THREE.CylinderGeometry();
-// var cylinder = new THREE.Mesh(geometry, material);
-// cylinder.position.x = 2;
-// scene.add(cylinder);
-//
-// geometry = new THREE.ConeGeometry();
-// var cone = new THREE.Mesh(geometry, material);
-// cone.position.x = -2;
-// scene.add(cone);
-//
-// geometry = new THREE.TorusGeometry();
-// var torus = new THREE.Mesh(geometry, material);
-// torus.position.y = 2;
-// scene.add(torus);
+geometry = new THREE.BoxGeometry(.3, 1, .3);
+object = new THREE.Mesh(geometry, material);
+object.position.x = -.5;
+object.position.y = -1.2;
+group.add(object);
+
+geometry = new THREE.SphereGeometry(.3, 12, 12);
+object = new THREE.Mesh(geometry, material);
+object.position.x = .5;
+object.position.y = -.45;
+group.add(object);
+
+geometry = new THREE.SphereGeometry(.3, 12, 12);
+object = new THREE.Mesh(geometry, material);
+object.position.x = -.5;
+object.position.y = -.45;
+group.add(object);
+
+// Create body
+geometry = new THREE.ConeGeometry(.7, 1, 16);
+object = new THREE.Mesh(geometry, material);
+object.position.y = .3;
+group.add(object);
+
+geometry = new THREE.CylinderGeometry(.7, .7, 1, 16);
+object = new THREE.Mesh(geometry, material);
+object.position.y = 1.3;
+group.add(object);
+
+// Create head
+geometry = new THREE.SphereGeometry(0.5, 32, 32);
+object = new THREE.Mesh(geometry, material);
+object.position.y = 2.3;
+group.add(object);
+
+geometry = new THREE.TorusGeometry(0.5, 0.1, 16, 12);
+object = new THREE.Mesh(geometry, material);
+object.position.y = 2.65;
+object.rotation.x = Math.PI / 2;
+group.add(object);
+
+// Create arms
+geometry = new THREE.SphereGeometry(.2, 12, 12);
+object = new THREE.Mesh(geometry, material);
+object.position.x = .8;
+object.position.y = 1.3;
+group.add(object);
+
+geometry = new THREE.SphereGeometry(.2, 12, 12);
+object = new THREE.Mesh(geometry, material);
+object.position.x = -.8;
+object.position.y = 1.3;
+group.add(object);
+
+geometry = new THREE.BoxGeometry(0.25, 0.9, 0.25);
+object = new THREE.Mesh(geometry, material);
+object.position.x = 1;
+object.position.y = 0.75;
+object.rotation.z = Math.PI / 8;
+group.add(object);
+
+geometry = new THREE.BoxGeometry(0.25, 0.9, 0.25);
+object = new THREE.Mesh(geometry, material);
+object.position.x = -1;
+object.position.y = 1.85;
+object.rotation.z = Math.PI / 8;
+group.add(object);
+
+geometry = new THREE.CylinderGeometry(0.13, 0.13, 0.5, 16);
+object = new THREE.Mesh(geometry, material);
+object.position.x = 1.2;
+object.position.y = 0.25;
+object.position.z = -0.1;
+object.rotation.x = Math.PI / 2;
+group.add(object);
+
+geometry = new THREE.CylinderGeometry(0.13, 0.13, 0.5, 16);
+object = new THREE.Mesh(geometry, material);
+object.position.x = -1.2;
+object.position.y = 2.35;
+object.position.z = 0.1;
+object.rotation.x = Math.PI / 2;
+group.add(object);
 
 var render = function() {
     requestAnimationFrame(render);
