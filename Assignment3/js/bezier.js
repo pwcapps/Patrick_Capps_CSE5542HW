1f02f66b6c0939a60ce7667803c6ef34268e1336
Patrick Capps <patrick.capps0@gmail.com>
2019-03-04 14:27:41 -0500

    create all points for surface

--- Assignment3/js/bezier.js
+++ Assignment3/js/bezier.js
@@ -79,17 +79,25 @@ function init() {
 
   }
 
-  var curve1 = [pointsArrays[0], pointsArrays[1], pointsArrays[2], pointsArrays[3]];
-  for ( i = 0; i <= 1; i += stepSize ) {
-    var p = evalBCurve(curve1, i);
-    console.log(p);
-    geometry = new THREE.SphereGeometry(0.05, 10, 10);
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false } );
-    var obj = new THREE.Mesh( geometry, material );
-    obj.position.set(p[0], p[1], p[2]);
-    scene.add(obj);
-  }
+  // var curve1 = [pointsArrays[0], pointsArrays[1], pointsArrays[2], pointsArrays[3]];
+  // for ( i = 0; i <= 1; i += stepSize ) {
+  //   var p = evalBCurve(curve1, i);
+  //   console.log(p);
+  //   geometry = new THREE.SphereGeometry(0.05, 10, 10);
+  //   material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false } );
+  //   var obj = new THREE.Mesh( geometry, material );
+  //   obj.position.set(p[0], p[1], p[2]);
+  //   scene.add(obj);
+  // }
 
+  for (u = 0; u <= 1; u += stepSize) {
+    for (v = 0; v <= 1; v += stepSize) {
+      var p = evalBSurface(pointsArrays, u, v);
+      console.log(p);
+      createPoint(p);
+    }
+  }
+  
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
@@ -99,6 +107,20 @@ function animate() {
   requestAnimationFrame( animate );
 
   renderer.render( scene, camera );
+}
+
+function evalBSurface(points, u, v) {
+    var uPoints = [];
+    for (i = 0; i < 4; i++) {
+      var curvePoints = [];
+      curvePoints[0] = points[i * 4];
+      curvePoints[1] = points[i * 4 + 1];
+      curvePoints[2] = points[i * 4 + 2];
+      curvePoints[3] = points[i * 4 + 3];
+      uPoints[i] = evalBCurve(curvePoints, u);
+    }
+
+    return evalBCurve(uPoints, v);
 }
 
 function evalBCurve(points, t) {
@@ -123,4 +145,12 @@ function pointAddition(p1, p2) {
 
   return [p1[0] + p2[0], p1[1] + p2[1], p1[2] + p2[2]];
 
+}
+
+function createPoint(p) {
+  geometry = new THREE.SphereGeometry(0.05, 10, 10);
+  material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false } );
+  var obj = new THREE.Mesh( geometry, material );
+  obj.position.set(p[0], p[1], p[2]);
+  scene.add(obj);
 }
